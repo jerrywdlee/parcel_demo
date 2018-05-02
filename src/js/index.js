f48c2afbb9acd357992766ab987819728404ca41
Jerry Lee <lee@idoctor.co.jp>
2018-05-02 14:22:19 +0900

    connect to demo server

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,17 @@ import io from 'socket.io-client';
 
 console.log("hello world");
 console.log(`jQuery Version: ${$.fn.jquery}`);
+
+const socket = io.connect('http://localhost:2048');
+socket.on('connect', function () {
+  console.log(`Session: ${socket.io.engine.id}`);
+
+  socket.on('text', function (text) {
+    console.log(text);
+  });
+
+  socket.on('notification', function (msg) {
+    console.log(msg);
+    $('.ol').append(`<li>${msg.text} ${msg.timestamp}</li>`)
+  });
+});
