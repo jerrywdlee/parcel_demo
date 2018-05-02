4f7d4b8f901fe93e59ba6d0e2af9be2a413972e9
Jerry Lee <lee@idoctor.co.jp>
2018-05-02 20:03:03 +0900

    add delay

--- socket_server/server.js
+++ socket_server/server.js
@@ -9,16 +9,21 @@ const PORT = 2048;
 const io = socket.listen(server);
 
 io.sockets.on('connection', function (client) {
-  console.log("New client ! " + client.id);
+  console.log(`New client ! ${client.id}`);
   client.emit('text', 'wow. such event. very real time.');
   client.on('message', function (data) {
     console.log('Message received ' + data.message);
     io.sockets.emit('message', { message: data.message });
   });
 
+  client.on('__ping', (timestamp) => {
+    client.emit('__pong', timestamp)
+    // console.log(timestamp);
+  });
+
   let emmiter = setInterval(function () {
     client.emit('notification', {text: 'Notification!!', timestamp: (new Date()).toISOString()});
-    console.log('Send notification', 'Notification!!');
+    // console.log('Send notification', 'Notification!!');
   }, 5000);
 
   client.on('disconnect', function () {
@@ -30,3 +35,26 @@ io.sockets.on('connection', function (client) {
 
 server.listen(PORT);
 console.log(`server on ${PORT}`);
+
+/**
+ * memo!:
+ * 
+ * TODO1:
+let dots = ''
+process.stdout.write(`Loading `)
+
+let tmrID = setInterval(() => {
+  dots += '.'
+  process.stdout.write(`\rLoading ${dots}`)
+}, 1000)
+
+setTimeout(() => {
+  clearInterval(tmrID)
+  console.log(`\rLoaded in [3500 ms]`)
+}, 3500)
+
+ * TODO2:
+ * use parcel as static server
+ * 
+ * 
+ */
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,57 @@
-'use strict';
+'use strict'
 
-import $ from 'jquery';
-import io from 'socket.io-client';
+import $ from 'jquery'
+import io from 'socket.io-client'
 
-console.log("hello world");
-console.log(`jQuery Version: ${$.fn.jquery}`);
+console.log("hello world")
+console.log(`jQuery Version: ${$.fn.jquery}`)
 
-const socket = io.connect('http://localhost:2048');
-socket.on('connect', function () {
-  console.log(`Session: ${socket.io.engine.id}`);
+const socket = io.connect('http://localhost:2048')
+const CONNECTION = {
+  socket: socket,
+  online: false,
+  rtt: Infinity
+}
 
-  socket.on('text', function (text) {
-    console.log(text);
-  });
+calcLatency(CONNECTION)
 
-  socket.on('notification', function (msg) {
-    console.log(msg);
-    $('.ol').append(`<li>${msg.text} ${msg.timestamp}</li>`)
-  });
-});
+socket.on('connect', () => {
+  console.log(`Session: ${socket.io.engine.id}`)
+  CONNECTION.online = true
+  socket.emit('__ping', Date.now())
+  socket.emit('message', { message: 'msg' });
+})
+
+socket.on('text', (text) => {
+  console.log(text)
+})
+
+socket.on('notification', (msg) => {
+  // console.log(msg)
+  $('.ol').append(`<li>${msg.text} ${msg.timestamp}</li>`)
+})
+
+setInterval(() => {
+  $('.delay').text(CONNECTION.rtt)
+  $('.status').text(CONNECTION.online ? 'Online': 'Offline')
+}, 500);
+
+function calcLatency (connection, cycleTime = 1000) {
+  setInterval(() => {
+    // const socket = connection.socket
+    let timeoutFlag = false
+    // console.log(connection);
+    if (!timeoutFlag && socket) {
+      socket.emit('__ping', Date.now())
+      timeoutFlag = true
+      socket.on('__pong', timestamp => {
+        connection.rtt = Date.now() - parseInt(timestamp)
+        timeoutFlag = false
+      })
+    } else {
+      connection.online = false
+      timeoutFlag = false
+    }
+  }, cycleTime)
+}
+
